7a643bcbd0aac0cd52c8cd18f130051740b25af7
Adrian Kayllan <kayllan.profissional@proton.me>
2025-07-02 16:50:49 -0500

    #

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            emal: req.body.name,
+            name: req.body.name,
+            age: req.bosy.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
 
 app.get('/usuarios', (req, res) => {
-    app.send('Ok, deu bom')
+    res.json(users)
 })
 
 app.listen(3000)
 
+/* 
+ankayllancb
+lGrndD2kHIAmWpHf
+*/
